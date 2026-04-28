892d28266285a8cf900939943f2ac919d252bf2d
gbmcpe <gbmcpe1@gmail.com>
2026-04-28 17:23:43 -0500

    Reorg / Deliverable 1 Basics

--- Week 5 Deliverables/Deliverable 1/Wk5Del1CreateStatement.sql
+++ Week 5 Deliverables/Deliverable 1/Wk5Del1CreateStatement.sql
@@ -2,6 +2,6 @@ CREATE TABLE SSIS_FILE_IMPORT(
     Currency1 Decimal(10),
     CurrencyType Varchar(3),
     RecordDate datetime PRIMARY KEY,
-    RecordTime time,
     Currency2 Decimal(10)
+    
 )
